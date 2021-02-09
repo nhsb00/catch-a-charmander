c15dff6b4bc8e025b8f205b9e6d5103387c91748
soobin <nhsb00@hotmail.com>
2021-02-08 23:39:19 -0500

    change position of object

--- src/target.js
+++ src/target.js
@@ -2,7 +2,7 @@ export default class Target {
     constructor(width, height) {
         this.targetWidth = Math.floor(Math.random() * 100 + 30);
         this.targetHeight = Math.floor(Math.random() * 100 + 10);
-        this.targetX = Math.floor(Math.random() * (500 - this.targetWidth) + 500);
+        this.targetX = Math.floor(Math.random() * (400 - this.targetWidth) + 400);
         this.targetY = height - this.targetHeight;
     }
 
